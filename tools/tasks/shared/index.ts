15a58ffc604873882db27fb99ba4f3084b3a62df
Alessian Dormal <alessian@alessian.be>
2025-04-02 22:36:09 +0200

    I swear if this becomes a 31 situation. I should've just told it to skip changelogs bro

--- tools/tasks/shared/index.ts
+++ tools/tasks/shared/index.ts
@@ -13,21 +13,15 @@ import {
 import { deleteAsync } from "del";
 import { FileDef } from "#types/fileDef.ts";
 import {
-	downloadFileDef,
 	downloadOrRetrieveFileDef,
-	isEnvVariableSet,
 	promiseStream,
-	shouldSkipChangelog,
 } from "#utils/util.ts";
 import transformVersion from "./transformVersion.ts";
-import { createBuildChangelog } from "../changelog/index.ts";
-import mustache from "mustache";
 import {
 	updateFilesBuildSetup,
 	updateLabsVersion,
 } from "../misc/transformFiles.ts";
 import { transformQuestBook } from "./quest.ts";
-import logInfo from "#utils/log.ts";
 
 async function sharedCleanUp() {
 	await deleteAsync(upath.join(sharedDestDirectory, "*"), { force: true });
@@ -112,88 +106,6 @@ async function fetchExternalDependencies() {
 /**
  * Either fetches the Changelog File, or makes one. Does nothing if 'SKIP_CHANGELOG' is set to a truthy value.
  */
-async function fetchOrMakeChangelog() {
-	if (shouldSkipChangelog()) return;
-
-	if (isEnvVariableSet("MADE_CHANGELOG")) {
-		let made = false;
-		try {
-			made = JSON.parse((process.env.MADE_CHANGELOG ?? "false").toLowerCase());
-		} catch (err) {
-			throw new Error("Made Changelog Env Variable set to Invalid Value.");
-		}
-
-		if (made) {
-			logInfo("Already Made Changelogs...");
-			return;
-		}
-	}
-
-	if (
-		isEnvVariableSet("CHANGELOG_URL") &&
-		isEnvVariableSet("CHANGELOG_CF_URL")
-	) {
-		logInfo("Using Changelog Files from URL.");
-		await downloadChangelogs(
-			process.env.CHANGELOG_URL ?? "",
-			process.env.CHANGELOG_CF_URL ?? "",
-		);
-		return;
-	}
-	if (isEnvVariableSet("CHANGELOG_BRANCH")) {
-		logInfo("Using Changelog Files from Branch.");
-		const url =
-			"https://raw.githubusercontent.com/Nomi-CEu/Nomi-CEu/{{ branch }}/{{ filename }}";
-		await downloadChangelogs(
-			mustache.render(url, {
-				branch: process.env.CHANGELOG_BRANCH,
-				filename: "CHANGELOG.md",
-			}),
-			mustache.render(url, {
-				branch: process.env.CHANGELOG_BRANCH,
-				filename: "CHANGELOG_CF.md",
-			}),
-		);
-		return;
-	}
-	logInfo("Creating Changelog Files.");
-	await createBuildChangelog();
-}
-
-async function downloadChangelogs(
-	changelogURL: string,
-	changelogCFURL: string,
-) {
-	const changelog = await downloadFileDef({ url: changelogURL });
-	const changelogCF = await downloadFileDef({ url: changelogCFURL });
-
-	await writeToChangelog(changelog, "CHANGELOG.md", changelogURL);
-	await writeToChangelog(changelogCF, "CHANGELOG_CF.md", changelogCFURL);
-}
-
-async function writeToChangelog(
-	buffer: Buffer,
-	changelogFile: string,
-	url: string,
-) {
-	let handle: fs.promises.FileHandle | undefined = undefined;
-	try {
-		handle = await fs.promises.open(
-			upath.join(buildConfig.buildDestinationDirectory, changelogFile),
-			"w",
-		);
-
-		await handle.write(buffer);
-		await handle.close();
-	} catch (err) {
-		if (handle && (await handle.stat()).isFile()) {
-			logInfo(`Couldn't download changelog from URL ${url}, cleaning up...`);
-
-			await handle.close();
-		}
-		throw err;
-	}
-}
 
 const updateBuildLabsVersion = async () => {
 	await updateLabsVersion(sharedDestDirectory);
@@ -204,18 +116,11 @@ export default gulp.series(
 	createSharedDirs,
 	copyOverrides,
 	copyPackModeSwitchers,
-	fetchOrMakeChangelog,
 	fetchExternalDependencies,
 	updateFilesBuildSetup,
 	updateBuildLabsVersion,
 	transformVersion,
 	transformQuestBook,
-);
-
-export const buildChangelog = gulp.series(
-	sharedCleanUp,
-	createSharedDirs,
-	fetchOrMakeChangelog,
 );
 
 export const typoBuild = gulp.series(
